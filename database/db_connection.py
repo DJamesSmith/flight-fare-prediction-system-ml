46a49e23dfd71e7ad2b4029aa6669611650fe717
Dion James Smith <dion.smith@live.com>
2026-07-04 17:10:18 +0530

    Implement database creation and table setup for flight fare prediction

--- database/db_connection.py
+++ database/db_connection.py
@@ -1,33 +1,109 @@
-# PostgreSQL connection
-# Executing SQL queries
-# CRUD operations
-# Returning model objects
+# # PostgreSQL connection
+# # Executing SQL queries
+# # CRUD operations
+# # Returning model objects
+
+# import os
+# import psycopg
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+# class DatabaseConnection:
+#     def __init__(self):
+#         self.connection = None
+
+#     def connect(self):
+#         if self.connection is None:
+#             self.connection = psycopg.connect(
+#                 host=os.getenv("DB_HOST"),
+#                 port=os.getenv("DB_PORT"),
+#                 dbname=os.getenv("DB_NAME"),
+#                 user=os.getenv("DB_USER"),
+#                 password=os.getenv("DB_PASSWORD")
+#             )
+#         return self.connection
+
+#     def disconnect(self):
+#         if self.connection is not None:
+#             self.connection.close()
+#             self.connection = None
+
+#     def get_connection(self):
+#         return self.connect()
 
 import os
-import psycopg
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 from dotenv import load_dotenv
+from database.queries import CREATE_USERS_TABLE, CREATE_FLIGHTS_TABLE, CREATE_PREDICTIONS_TABLE
 
 load_dotenv()
 
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+
+# Creates the project database if it does not already exist
+def create_database():
+    connection = psycopg2.connect(
+        host=DB_HOST,
+        database="postgres",
+        user=DB_USER,
+        password=DB_PASSWORD,
+        port=DB_PORT
+    )
+
+    connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    cursor = connection.cursor()
+    cursor.execute("SELECT 1 FROM pg_database WHERE datname=%s;", (DB_NAME,))
+    database_exists = cursor.fetchone()
+    if not database_exists:
+        cursor.execute(f"CREATE DATABASE {DB_NAME}")
+        print(f"Database '{DB_NAME}' created successfully.")
+    else:
+        print(f"Database '{DB_NAME}' already exists.")
+
+    cursor.close()
+    connection.close()
+
+# Creates all required project tables
+def create_tables():
+    with DatabaseConnection() as cursor:
+        cursor.execute(CREATE_USERS_TABLE)
+        cursor.execute(CREATE_FLIGHTS_TABLE)
+        cursor.execute(CREATE_PREDICTIONS_TABLE)
+
+    print("All database tables are ready.")
+
+# Custom Context Manager responsible only for opening and closing PostgreSQL connections
 class DatabaseConnection:
     def __init__(self):
         self.connection = None
+        self.cursor = None
 
-    def connect(self):
-        if self.connection is None:
-            self.connection = psycopg.connect(
-                host=os.getenv("DB_HOST"),
-                port=os.getenv("DB_PORT"),
-                dbname=os.getenv("DB_NAME"),
-                user=os.getenv("DB_USER"),
-                password=os.getenv("DB_PASSWORD")
-            )
-        return self.connection
+    def __enter__(self):
+        self.connection = psycopg2.connect(
+            host=DB_HOST,
+            database=DB_NAME,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            port=DB_PORT
+        )
+        self.cursor = self.connection.cursor()
+        return self.cursor
 
-    def disconnect(self):
-        if self.connection is not None:
+    def __exit__(self, exc_type, exc_value, traceback):
+        if exc_type:
+            print(f"Database Error: {exc_value}")
+            self.connection.rollback()
+        else:
+            self.connection.commit()
+
+        if self.cursor:
+            self.cursor.close()
+
+        if self.connection:
             self.connection.close()
-            self.connection = None
-
-    def get_connection(self):
-        return self.connect()
---
+++ database/queries.py
@@ -0,0 +1,44 @@
+CREATE_USERS_TABLE = """
+CREATE TABLE IF NOT EXISTS users(
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(20) NOT NULL CHECK(role IN ('Admin', 'User')),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+"""
+
+CREATE_FLIGHTS_TABLE = """
+CREATE TABLE IF NOT EXISTS flights(
+    flight_id SERIAL PRIMARY KEY,
+    airline VARCHAR(100) NOT NULL,
+    source VARCHAR(100) NOT NULL,
+    destination VARCHAR(100) NOT NULL,
+    journey_date DATE NOT NULL,
+    departure_time TIME NOT NULL,
+    arrival_time TIME NOT NULL,
+    duration VARCHAR(50) NOT NULL,
+    total_stops VARCHAR(30) NOT NULL,
+    additional_information VARCHAR(255)
+);
+"""
+
+CREATE_PREDICTIONS_TABLE = """
+CREATE TABLE IF NOT EXISTS predictions(
+    prediction_id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    flight_id INTEGER NOT NULL,
+    predicted_fare DECIMAL(10,2) NOT NULL,
+    prediction_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_prediction_user
+        FOREIGN KEY(user_id)
+        REFERENCES users(user_id)
+        ON DELETE CASCADE,
+
+    CONSTRAINT fk_prediction_flight
+        FOREIGN KEY(flight_id)
+        REFERENCES flights(flight_id)
+        ON DELETE CASCADE
+);
+"""
